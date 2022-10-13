0f0760258984fab21c8e865e9951738e9486cf96
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-10-12 21:52:31 -0500

    u

--- src/gestorAplicacion/Hija/ContactoUsuario.java
+++ src/gestorAplicacion/Hija/ContactoUsuario.java
@@ -1,10 +1,21 @@
 package gestorAplicacion.Hija;
+
 import gestorAplicacion.Padre.Contactoboolean;
+
 public class ContactoUsuario extends Contactoboolean {
 	private String NumeroTelefonoContacto;
+	
 	public ContactoUsuario(Integer numeroid,Usuario usuario,String nombrecontacto,String numerotelefono) {
 		super(numeroid,usuario,nombrecontacto);
 		this.NumeroTelefonoContacto=numerotelefono;
+	}
+	
+	public String getNumeroTelefonoContacto() {
+		return this.NumeroTelefonoContacto;
+	}
+	
+	public void setNumeroTelefonoContacto(String cambiar) {
+		this.NumeroTelefonoContacto=cambiar;
 	}
 
 }
--- src/gestorAplicacion/Hija/ContactosPendientes.java
+++ src/gestorAplicacion/Hija/ContactosPendientes.java
@@ -1,9 +1,21 @@
 package gestorAplicacion.Hija;
 
-public class ContactosPendientes {
+import gestorAplicacion.Padre.Contactoboolean;
 
-	public ContactosPendientes() {
-		// TODO Auto-generated constructor stub
+public class ContactosPendientes extends Contactoboolean {
+
+private String EmailContacto;
+	
+	public ContactosPendientes(Integer numeroid,Usuario usuario,String nombrecontacto,String emailcontacto) {
+		super(numeroid,usuario,nombrecontacto);
+		this.EmailContacto=emailcontacto;
 	}
-
+	
+	public String getEmailContacto() {
+		return this.EmailContacto;
+	}
+	
+	public void setEmailContacto(String cambiar) {
+		this.EmailContacto=cambiar;
+	}
 }
