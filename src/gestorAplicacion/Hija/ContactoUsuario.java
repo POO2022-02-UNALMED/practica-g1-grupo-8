517643290637b69e5b627e80e6a9b9505672afab
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-10-12 21:36:59 -0500

    j

--- src/gestorAplicacion/Hija/ContactoUsuario.java
+++ src/gestorAplicacion/Hija/ContactoUsuario.java
@@ -1,5 +1,10 @@
 package gestorAplicacion.Hija;
-
-public class ContactoUsuario {
+import gestorAplicacion.Padre.Contactoboolean;
+public class ContactoUsuario extends Contactoboolean {
+	private String NumeroTelefonoContacto;
+	public ContactoUsuario(Integer numeroid,Usuario usuario,String nombrecontacto,String numerotelefono) {
+		super(numeroid,usuario,nombrecontacto);
+		this.NumeroTelefonoContacto=numerotelefono;
+	}
 
 }
