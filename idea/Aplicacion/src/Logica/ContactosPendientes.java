54d484e6e7d9b14de0b14ded7ec71e55f0186da5
SebastianSotoAr <ssotoa@unal.edu.co>
2022-10-14 19:22:54 -0500

    idea 3

--- idea/Aplicacion/src/Logica/ContactosPendientes.java
+++ idea/Aplicacion/src/Logica/ContactosPendientes.java
@@ -1,14 +1,14 @@
 package Logica;
 
-public class ContactosPendientes extends Contacto{
+class ContactosPendientes extends Contacto{
 	private final String targeta;
 
-	public ContactosPendientes(String nombre, String email, String nombreCompleto) {
+	protected ContactosPendientes(String nombre, String email, String nombreCompleto) {
 		super(nombre, email, nombreCompleto);
 		this.targeta = "No hay mensajes";
 	}
 
-	public String getTargeta() {
+	protected String getTargeta() {
 		return targeta;
 	}
 
