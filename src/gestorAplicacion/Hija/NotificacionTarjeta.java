513592084d2a27059f71bf35912a8fe5aba0f8b1
jomolinav <jomolinav@unal.edu.co>
2022-10-16 12:52:07 -0500

    NotTarjeta - 1

--- src/gestorAplicacion/Hija/NotificacionTarjeta.java
+++ src/gestorAplicacion/Hija/NotificacionTarjeta.java
@@ -1,4 +1,29 @@
 package gestorAplicacion.Hija;
 
+import java.util.ArrayList;
+
 public class NotificacionTarjeta {
+	private ArrayList<TarjetaSocial> tarjetasSocialesLeido;
+	private ArrayList<TarjetaNegocios> tarjetasNegociosLeido;
+	private ArrayList<TarjetaSocial> tarjetasSocialesNoLeido;
+	private ArrayList<TarjetaNegocios> tarjetasNegociosNoLeido
+	
+	public NotificacionTarjeta(ArrayList<TarjetaSocial> tarjetasSociales,
+			ArrayList<TarjetaNegocios> tarjetasNegociosLeido,
+			ArrayList<TarjetaSocial> tarjetasSocialesNoLeido,
+			ArrayList<TarjetaNegocios> tarjetasNegociosNoLeido) {
+		
+		this.tarjetasSocialesLeido = tarjetasSocialesLeido;
+		this.tarjetasNegociosLeido = tarjetasNegociosLeido;
+		this.tarjetasSocialesNoLeido = tarjetasSocialesNoLeido;
+		this.tarjetasNegociosNoLeido = tarjetasNegociosNoLeido;
+	}
+	
+	public ArrayList<TarjetaSocial> getTarjetasSocialesLeido(){
+		return tarjetasSocialesLeido;
+	}
+	public ArrayList<TarjetaNegocios> getTarjetasNegociosLeido(){
+		return tarjetasNegociosLeido;
+	}
+	
 }
