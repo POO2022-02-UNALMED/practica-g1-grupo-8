4023431fe4427dbd28ea7a100cc6a8cce3108a71
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-10-15 20:02:18 -0500

    j

--- src/gestorAplicacion/Hija/ContactosPendientes.java
+++ src/gestorAplicacion/Hija/ContactosPendientes.java
@@ -3,8 +3,6 @@ package gestorAplicacion.Hija;
 import gestorAplicacion.Padre.ContactoTarjetas;
 
 public class ContactosPendientes extends ContactoTarjetas {
-
-private String EmailContacto;
 	
 	public ContactosPendientes(Integer numeroid,Usuario usuario,String nombrecontacto,String emailcontacto) {
 		super(numeroid,usuario,nombrecontacto);
