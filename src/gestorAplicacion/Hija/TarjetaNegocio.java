87ec4e48b44d46e175f7071a95c7d7610794ee6b
upar1234 <upar1234@gmail.com>
2022-10-18 22:18:43 -0500

    Update TarjetaNegocio.java

--- src/gestorAplicacion/Hija/TarjetaNegocio.java
+++ src/gestorAplicacion/Hija/TarjetaNegocio.java
@@ -1,11 +1,14 @@
 package gestorAplicacion.Hija;
 
-public class TarjetaNegocio {
+public class TarjetaNegocio extends gestorAplicacion.Padre.Tarjeta{
     private String logo;
     private String descripcion;
     private String terminos;
     private String nombreCompletoA;
     private String getNombreCompletoB;
 
-    public TarjetaNegocio(int id, )
+    public TarjetaNegocio(String emailA, String emailB, String titulo, String cuerpo, String logo, String descripcion, String terminos, String nombreCompletoA, String getNombreCompletoB){
+        super(emailA, emailB, titulo, cuerpo);
+
+    }
 }
