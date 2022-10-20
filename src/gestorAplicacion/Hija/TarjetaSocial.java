65ad9f68754a6fb0aee3a21e2d300bbefef17c5b
upar1234 <upar1234@gmail.com>
2022-10-19 23:50:32 -0500

    s

--- src/gestorAplicacion/Hija/TarjetaSocial.java
+++ src/gestorAplicacion/Hija/TarjetaSocial.java
@@ -1,5 +1,16 @@
 package gestorAplicacion.Hija;
 
-public class TarjetaSocial {
+public class TarjetaSocial extends gestorAplicacion.Padre.Tarjeta {
+    private String nombreA;
+    private String nombreB;
 
+    public TarjetaSocial(String emailA, String emailB, String titulo, String cuerpo, String nombreA, String nombreB){
+        super(emailA, emailB, titulo, cuerpo);
+        this.nombreA=nombreA;
+        this.nombreB=nombreB;
+    }
+    public TarjetaSocial(String emailA, String emailB, String nombreA, String nombreB){
+        super(emailA,emailB);
+
+    }
 }
