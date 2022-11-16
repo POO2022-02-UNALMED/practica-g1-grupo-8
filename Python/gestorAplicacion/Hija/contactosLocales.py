1fabe4dabe77eb473ec35d5e6c93b6c8c7fb8754
jzapatamo <111089101+jzapatamo@users.noreply.github.com>
2022-11-16 17:55:20 -0500

    contactos locales python

--- Python/gestorAplicacion/Hija/contactosLocales.py
+++ Python/gestorAplicacion/Hija/contactosLocales.py
@@ -1,12 +1,15 @@
+from Padre.Contacto import Contacto
+from Padre.Mensaje import Mensaje
 #Los contactos locales son personas con las que ya se inicio una convesacion
 class ContactosLocales(Contacto):
 
+    _mensajes: list[Mensaje]
     #Genera los contactos locales con la capacidad de recibir mensajes
     def __init__(self, nombre, email, nombreCompleto):
         super().__init__(nombre, email, nombreCompleto)
         self._mensajes = []
 
-    def getMensajes(self):
+    def getMensajes(self) -> list[Mensaje]:
         return self._mensajes
 
     def setMensajes(self, mensajes):
