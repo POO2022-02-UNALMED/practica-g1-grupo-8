78ab4a125b578d46ab503a63ebfdcff778fea84b
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-11-26 17:11:35 -0500

    nn

--- Python/uiMain/paginaInicio.py
+++ Python/uiMain/paginaInicio.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 class ventana():
+    juanpabloG=""""""
+    alejandroA="hh"
+    sebastianS=""
+    juansimonZ="{"
+    josemanuelM="++"
+    lista=[juanpabloG,alejandroA,sebastianS,juansimonZ,josemanuelM]
     
     def __init__(self) -> None:
         self.ventana=tk.Tk()
@@ -10,9 +16,8 @@ class ventana():
         self.ventana.config(menu=self.menuBar)
 
         self.menuBar.add_command(label="Salir",command=lambda : self.ventana.destroy())
-        def descripcion(self):
-            self.textdescripcion.config(text="hola")
-        self.menuBar.add_command(label="Descripcion del sistema",command=lambda:descripcion())
+        
+        self.menuBar.add_command(label="Descripcion del sistema",command=lambda:self.descripcionn())
 
         self.P1=tk.Frame(self.ventana)
         self.P1.pack(side="left",padx=10,pady=5,fill="both")
@@ -21,7 +26,7 @@ class ventana():
         self.P3.pack(side="top")
         self.saludo=tk.Label(self.P3,text="Bienvenido a este programa",font=("Roman",30))
         self.saludo.pack(side="top",padx=5,pady=5)
-        self.descripcion=tk.Label(self.P3,text="")
+        self.descripcion=tk.Label(self.P3,text="s")
         self.descripcion.pack(side="bottom",padx=5,pady=5)
 
         self.P4=tk.Frame(self.P1)
@@ -35,19 +40,8 @@ class ventana():
 
         self.P5=tk.Frame(self.P2)
         self.P5.pack(side="top")
-        juanpabloG="sss"
-        alejandroA="hh"
-        sebastianS=""
-        juansimonZ="{"
-        josemanuelM="++"
-
-        lista=[juanpabloG,alejandroA,sebastianS,juansimonZ,josemanuelM]
-        def mas(self):
-            self.contador=+1
-        def boton(self):
-            mas()
-            self.textovida.config(text=lista[self.contador])
-        self.textovida=tk.Button(self.P5,text=lista[self.contador],command=boton)
+       
+        self.textovida=tk.Button(self.P5,text=self.lista[self.contador],command=self.boton)
         self.textovida.pack(fill="both")
         self.textovida.config(relief="flat")
 
@@ -57,6 +51,16 @@ class ventana():
         self.P6.pack(side="bottom")
 
         self.ventana.mainloop()
+    def descripcionn(self):
+            self.descripcion.config(text="hola")
+    def mas(self):
+        if self.contador<4:
+            self.contador=self.contador+1
+        else:
+            self.contador=0
+    def boton(self):
+        self.mas()
+        self.textovida.config(text=self.lista[self.contador])
 
 p=ventana()
 
