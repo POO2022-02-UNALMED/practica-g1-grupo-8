3b2f421b251829c4b05b2b348fa1a5093bce8250
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-11-25 19:05:08 -0500

    j

--- Python/uiMain/paginaInicio.py
+++ Python/uiMain/paginaInicio.py
@@ -12,10 +12,21 @@ P1.pack(side="left",padx=10,pady=5,fill="both")
 
 P3=tk.Frame(P1)
 P3.pack(side="top")
-saludo=tk.Label(P3,text="")
+saludo=tk.Label(P3,text="Bienvenido a este programa")
 
 P4=tk.Frame(P1)
 P4.pack(side="bottom")
 
+botonventanprincipal=tk.Button(P4,text="Ir a la ventana principal")
+botonventanprincipal.pack(side="bottom")
+
 P2=tk.Frame(ventana)
 P2.pack(side="right",padx=10,pady=5,fill="both")
+
+P5=tk.Frame(P2)
+P5.pack(side="top")
+
+
+P6=tk.Frame(P1)
+P6.pack(side="bottom")
+ventana.mainloop()
