e58ad987fdc13172fa66300bd646a0a3eb5126ab
jomolinav <jomolinav@unal.edu.co>
2022-11-27 21:06:34 -0500

    ReseñaHojaJMMV

--- Python/uiMain/paginaInicio.py
+++ Python/uiMain/paginaInicio.py
@@ -8,7 +8,9 @@ class ventana():
     Persona proactiva, respetuosa y trabajadora que le gusta velar por el bien del equipo,
     le gusta aprender temas nuevos,la tecnología y  como funcionan las cosas,
     curioso y dispuesto a aceptar retos."""
+
     alejandroA="Hoja de vida no agregada por el participante, imágenes por defecto añadidas por el grupo."
+
     sebastianS="""Me considero una persona responable,
     dinánmica y creativa, con facilidad de
     adaptación y capacidad de trabajar en
@@ -16,6 +18,7 @@ class ventana():
     con iniciativa para resolver problemas
     eficientemente y lograr las metas y
     objetivos trazados por la empreza"""
+    
     juansimonZ="Hoja de vida no agregada por el participante, imágenes por defecto añadidas por el grupo."
     josemanuelM="Hoja de vida no agregada por el participante, imágenes por defecto añadidas por el grupo."
     lista=[juanpabloG,alejandroA,sebastianS,juansimonZ,josemanuelM]
