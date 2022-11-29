3c33f3d384fd2f8210f09d4b25a1eb5df8104c1c
simonzm7 <simonzap30@gmail.com>
2022-11-29 01:47:41 -0500

    editar perfil

--- Python/uiMain/framesFuncionalidades/cargarUsuario.py
+++ Python/uiMain/framesFuncionalidades/cargarUsuario.py
@@ -1,9 +1,12 @@
 from gestorAplicacion.Hija.usuario import Usuario
+from gestorAplicacion.Hija.tarjetaNegocioUsuario import TarjetaNegocioUsuario
+
 class CargarUsuario:
     usuario: Usuario
 
     @staticmethod
     def crearUsuario():
-        CargarUsuario.usuario = Usuario("Andres", "andres@email.com", "Andres Gomez", "Facebook", "Red Social", "Prohibido usar informacion falsa")
+        tarjetaNegocio = TarjetaNegocioUsuario("Facebook", "Red Social", "0000000000", "Calle 0 # 0 - 0")
+        CargarUsuario.usuario = Usuario("Andres", "andres@email.com", "Andres Gomez", tarjetaNegocio.getNombreNegocio(),tarjetaNegocio.getDescripcionNegocio(), "Prohibido usar informacion falsa", tarjetaNegocio)
 
     
